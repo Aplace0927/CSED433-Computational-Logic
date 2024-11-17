05fae953e65481e472497e81cfa7323b63c7136a
Aplace <aplace030927@gmail.com>
2024-11-17 15:01:19 +0900

    feat(hw07-prog): finish parentheses

--- HW/HW7_Programming/coq6.v
+++ HW/HW7_Programming/coq6.v
@@ -34,10 +34,17 @@ Notation LPs := (cons LP eps).
 Notation RPs := (cons RP eps).
 
 Inductive mparen : T -> Prop :=
+| meps : mparen eps
+| mpar : forall (mp: T), mparen mp -> mparen (LPs +++ mp +++ RPs)
+| mseq : forall (mp: T) (mp': T), mparen mp -> mparen mp' -> mparen (mp +++ mp').
 
 Inductive lparen : T -> Prop :=
+| leps : lparen eps
+| lseq : forall (lp: T) (lp': T), lparen lp -> lparen lp' -> lparen (LPs +++ lp +++ RPs +++ lp').
 
 Inductive tparen : T -> Prop :=
+| teps : tparen eps
+| tseq : forall (tp: T) (tp': T), tparen tp -> tparen tp' -> tparen (tp +++ LPs +++ tp' +++ RPs).
 
 (*
 Suppose that you want to prove the following lemma:
@@ -89,21 +96,129 @@ tparenConcat.  If you find yourself trying the 'generalize dependent' tactic,
 you could rewrite rewriting the theorem and dispense with the use of the
 'generalize dependent' tactic. 
 *)
+Lemma eps_left : forall s:T, eps +++ s = s.
+Proof.
+  intros s.
+  simpl.
+  reflexivity.
+Qed.
 
+Lemma eps_right : forall s:T, s +++ eps = s.
+Proof.
+  intros s.
+  induction s.
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite -> IHs.
+    reflexivity.
+Qed.
+
+Lemma par_assoc : forall s s' s'':T, s +++ s' +++ s'' = s +++ (s' +++ s'').
+Proof.
+  intros s s' s''.
+  induction s.
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite -> IHs.
+    reflexivity.
+Qed.
+
+
+Lemma tparen_concat : forall (t t':T), tparen t -> tparen t' -> tparen (t +++ t').
+  Proof.
+  intros t t' IHt IHt'.
+  generalize dependent t.
+  generalize dependent t'.
+  intros t' IHt'.
+  induction IHt'.
+  - intros t IHt.
+    simpl.
+    rewrite -> eps_right.
+    assumption.
+  - intros t IHt.
+    rewrite <- par_assoc.
+    rewrite <- par_assoc.
+    rewrite <- par_assoc.
+    apply (tseq (t +++ tp) (tp')).
+    + apply IHIHt'1.
+      assumption.
+    + assumption.
+Qed.
+
+Theorem lparen_concat: forall l l':T, lparen l -> lparen l' -> lparen (l +++ l').
+Proof.
+  intros l l' IHl IHl'.
+  generalize dependent l'.
+  generalize dependent l.
+  intros l IHl.
+  induction IHl.
+  - intros l' IHl'.
+    simpl.
+    assumption.
+  - intros l' IHl'.
+    rewrite -> par_assoc.
+    apply (lseq (lp) (lp' +++ l')).
+    + assumption.
+    + apply IHIHl2.
+      assumption.
+Qed.
+      
 Theorem mparen2lparen : forall s:T, mparen s -> lparen s.
 Proof.
+  intros m IHm.
+  induction IHm.
+  - apply leps.
+  - rewrite <- eps_right.
+    apply lseq.
+    + assumption.
+    + apply leps.
+  - apply lparen_concat.
+    + assumption.
+    + assumption.
 Qed.
 
 Theorem lparen2mparen : forall s:T, lparen s -> mparen s.
 Proof.
+  intros l IHl.
+  induction IHl.
+  - apply meps.
+  - apply (mseq (LPs +++ lp +++ RPs) (lp')).
+    + apply mpar.
+      assumption.
+    + assumption.
 Qed.
 
 Theorem mparen2tparen : forall s:T, mparen s -> tparen s.
 Proof.
+  intros t IHt.
+  induction IHt.
+  - apply teps.
+  - rewrite <- eps_left.
+    rewrite <- par_assoc.
+    rewrite <- par_assoc.
+    apply (tseq (eps) (mp)).
+    + apply teps.
+    + assumption.
+  - apply tparen_concat.
+    + assumption.
+    + assumption.
 Qed.
 
 Theorem tparen2lparen : forall s:T, tparen s -> lparen s.
 Proof.
+  intros t IHt.
+  apply mparen2lparen.
+  induction IHt.
+  - apply meps.
+  - rewrite -> par_assoc.
+    rewrite -> par_assoc.
+    rewrite <- (par_assoc (LPs) (tp') (RPs)).
+    apply (mseq (tp) (LPs +++ tp' +++ RPs)).
+    + assumption.
+    + apply mpar.
+      assumption.
 Qed.
 
 End Paren.
